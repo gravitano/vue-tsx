cae923e8141d750dce290b2ca7b57dfc68d858b5
Warsono <warsono16694@gmail.com>
2022-05-18 22:57:05 +0700

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { defineComponent } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
+import './index.css';
 
 const Header = defineComponent(() => {
   return () => (
-    <div>
-      <a href="#">Vue 3 JSX</a>
+    <div class="py-3 border-b bg-white">
+      <div class="container mx-auto flex gap-4 justify-between">
+        <RouterLink to="/" class="font-semibold text-lg">
+          Vue 3 JSX
+        </RouterLink>
 
-      <div>
-        <RouterLink to={'/'}>Home</RouterLink>
-        <RouterLink to={'/counter'}>Counter</RouterLink>
+        <div class="flex gap-3">
+          <RouterLink to={'/'}>Home</RouterLink>
+          <RouterLink to={'/counter'}>Counter</RouterLink>
+        </div>
       </div>
     </div>
   );
@@ -19,7 +24,9 @@ export default defineComponent(() => {
     <>
       <Header />
 
-      <RouterView />
+      <div class="container mx-auto py-2">
+        <RouterView />
+      </div>
     </>
   );
 });
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent(() => {
   const todos = ref([
@@ -20,13 +20,39 @@ export default defineComponent(() => {
     });
   };
 
+  const completed = computed(() =>
+    todos.value.filter((item) => item.isCompleted)
+  );
+
   return () => (
     <div>
-      {todos.value.map((todo) => (
-        <li>{todo.text}</li>
-      ))}
-      <input type="text" v-model={newTodo.value} />
-      <button onClick={addTodo}>Add</button>
+      <h1 class="font-semibold text-2xl mb-4">Counter</h1>
+
+      <input
+        placeholder="Type something..."
+        type="text"
+        v-model={newTodo.value}
+        class="px-3 py-2 border border-gray-300 rounded-md mr-3"
+      />
+      <button
+        class="rounded-md px-3 py-2 border border-blue-600 bg-blue-600 text-white hover:bg-blue-700"
+        onClick={addTodo}
+      >
+        Add
+      </button>
+
+      <div class="text-sm mt-3 text-gray-600">
+        Competed: ({completed.value.length})
+      </div>
+
+      <div class="mt-3">
+        {todos.value.map((todo) => (
+          <div class="flex gap-4 items-center">
+            <span>{todo.text}</span>
+            <input type="checkbox" v-model={todo.isCompleted} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 });
